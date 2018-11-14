6f6a53f38a3c901bc07f5d47c6be54bbf4ad1ed7
Janmejay Kumar <janmejay.arya@outlook.com>
2018-11-14 13:09:31 +0530

    Added New Code

--- SpecFlowProject/AmazonProductSearch.cs
+++ SpecFlowProject/AmazonProductSearch.cs
@@ -13,32 +13,27 @@ namespace SpecFlowProject
     [Binding]
     public sealed class AmazonProductSearch
     {
-        public IWebDriver driver;
+        private IWebDriver driver;
         // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
 
         [Given("Launch The Application and Navigate to URL")]
         public void LaunchTheApplicationAndNavigateURL()
         {
-            IWebDriver driver = new ChromeDriver();
+            this.driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.amazon.com");
-            System.Threading.Thread.Sleep(20000);
-            driver.FindElement(By.XPath("//*[@id='twotabsearchtextbox']")).SendKeys("Iphone");
-            System.Threading.Thread.Sleep(3000);
-            driver.FindElement(By.XPath("//*[@value='submit']")).Click();
-
-
         }
 
         [When("Check the Title page and put all the links in list")]
         public void checkTitlePageAndStoreLinksInList()
         {
             //IWebDriver driver;
-            
-            //Assert.Equals("Amazon", driver.FindElement(By.XPath("//span[text()='Amazon']")).Text);
-
-            
-           
-            
+            System.Threading.Thread.Sleep(20000);
+            //Assert.Equals("Amazon", this.driver.FindElement(By.XPath("//span[text()='Amazon']")).Text);
+            // Search the product in EditBox
+            this.driver.FindElement(By.XPath("//*[@id='twotabsearchtextbox']")).SendKeys("Iphone");
+        
+            System.Threading.Thread.Sleep(3000);
+            this.driver.FindElement(By.XPath("//*[@value='Go']")).Click();
         }
 
         [Then("the result should be (.*) on the screen")]
