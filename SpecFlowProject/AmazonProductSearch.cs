44203a1566a12854e3f6b94cf4c13b3f46d0a1cf
Janmejay Kumar <janmejay.arya@outlook.com>
2018-11-14 13:30:50 +0530

    Updsatdc

--- SpecFlowProject/AmazonProductSearch.cs
+++ SpecFlowProject/AmazonProductSearch.cs
@@ -14,6 +14,7 @@ namespace SpecFlowProject
     public sealed class AmazonProductSearch
     {
         private IWebDriver driver;
+        
         // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
 
         [Given("Launch The Application and Navigate to URL")]
@@ -21,19 +22,28 @@ namespace SpecFlowProject
         {
             this.driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.amazon.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8);
         }
 
         [When("Check the Title page and put all the links in list")]
         public void checkTitlePageAndStoreLinksInList()
         {
             //IWebDriver driver;
-            System.Threading.Thread.Sleep(20000);
+            //system.threading.thread.sleep(20000);
             //Assert.Equals("Amazon", this.driver.FindElement(By.XPath("//span[text()='Amazon']")).Text);
             // Search the product in EditBox
             this.driver.FindElement(By.XPath("//*[@id='twotabsearchtextbox']")).SendKeys("Iphone");
         
             System.Threading.Thread.Sleep(3000);
             this.driver.FindElement(By.XPath("//*[@value='Go']")).Click();
+
+            ICollection<IWebElement> links = driver.FindElements(By.TagName("a"));
+            foreach (IWebElement link in links)
+            {
+                Console.WriteLine(link.Text.ToString());
+            }
+
+
         }
 
         [Then("the result should be (.*) on the screen")]
